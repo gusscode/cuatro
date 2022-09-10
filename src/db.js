a484a82066cce80aca7532669b237f03aa829f8d
gusscode <gustavo_violinmus@hotmail.com>
2022-09-10 16:21:01 -0400

    db cambiado la forma

--- src/db.js
+++ src/db.js
@@ -1,15 +1,20 @@
 const mongoose = require('mongoose')
 const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/lavado'
-const connectDB =()=>{
-    mongoose.connect(MONGO_URI, { 'useNewUrlParser': true,
-        'useUnifiedTopology': true
-    })
-    .then(() => {
-        console.log('Conectado a MongoDB')
-    })
-    .catch(err => {
-        console.log(err)
-    })
+const connectDB =async ()=>{
+    try {
+        const db = await mongoose.connect(MONGO_URI);
+        console.log("Connected to ", db.connection.name);
+      } catch (error) {
+        console.error(error);
+      }
+      
+      mongoose.connection.on("connected", () => {
+        console.log("Mongoose is connected");
+      });
+      
+      mongoose.connection.on("disconnected", () => {
+        console.log("Mongoose is disconnected");
+      });
 
 } 
 
