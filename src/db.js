d6da64fd7c92267be8b673853603c58cf9c3a243
gusscode <gustavo_violinmus@hotmail.com>
2022-09-10 16:17:16 -0400

    aplicando antiguas configuraciones para mongo lts

--- src/db.js
+++ src/db.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose')
 const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/lavado'
 const connectDB =()=>{
-    mongoose.connect(MONGO_URI, { useNewUrlParser: true })
+    mongoose.connect(MONGO_URI, { 'useNewUrlParser': true,
+        'useUnifiedTopology': true
+    })
     .then(() => {
         console.log('Conectado a MongoDB')
     })
